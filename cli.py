e4326d5cb54e680fd03e4551da7aba44bf703034
andrei G <sinezub@yandex.ru>
2024-12-02 21:57:07 +0300

    Clean up cli.py

--- cli.py
+++ cli.py
@@ -7,22 +7,18 @@ def render_axis(points: list[tuple[int, int]]):
 
 def printer(size: tuple[int, int], points = None):
     x_max, y_max = size
-    boarders = [*['|'] * y_max, *['_'] * x_max]
+    borders = [*['|'] * y_max, *['_'] * x_max]
     points = render_axis(points or [])
-    print(y_max)
-    for index, boarder in enumerate(boarders):
+    print(f'{y_max}')
+    for index, border in enumerate(borders):
         x = points.get(y_max - index, '')
-        if boarder == '|':
-            end = '' if boarders[index + 1] == '_' else '\n'
-            print(boarder + x, end=end)
-        elif boarder == '_':
-            print(boarder, end='')
-    print(x_max)
+        end = '' if border == '_' or borders[index + 1] == '_' else '\n'
+        print(border + x, end=end)
+    print(f'{x_max}')
 
 
-points = [(15, 10), (5,7), (9,9)]
 
-def show():
+if __name__ == '__main__':
+    # TODO: read points from console args. Like, --points 3,4 1,1 5,5
+    points = [(1, 1), (15, 10), (5,7), (9,9)]
     printer((20, 10), points)
-
-show()
