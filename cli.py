e27d84e042ced3398f661d34746b1a450545c446
andrei G <sinezub@yandex.ru>
2024-12-02 21:49:44 +0300

    Fixed cli chart renderer.

--- cli.py
+++ cli.py
@@ -1,31 +1,28 @@
 from typing import Generator
-y_max = 10
-x_max = 10
 
 
 def render_axis(points: list[tuple[int, int]]):
     return {y: ' ' * (x - 1) + '+' for x, y in points}
 
-boarders = lambda x, y: [*['|'] * y, *['_'] * x]
 
-def printer(boarders: list[str], points = None):
-    points = points or []
-    points = render_axis(points)
+def printer(size: tuple[int, int], points = None):
+    x_max, y_max = size
+    boarders = [*['|'] * y_max, *['_'] * x_max]
+    points = render_axis(points or [])
+    print(y_max)
     for index, boarder in enumerate(boarders):
         x = points.get(y_max - index, '')
         if boarder == '|':
-            if boarders[index + 1] == '_':
-                print(boarder + x, end = '')
-            else:
-                print(boarder + x)
+            end = '' if boarders[index + 1] == '_' else '\n'
+            print(boarder + x, end=end)
         elif boarder == '_':
             print(boarder, end='')
+    print(x_max)
 
-points = [(-2,-3), (5,7), (9,9)]
-print(render_axis(points))
 
+points = [(15, 10), (5,7), (9,9)]
 
 def show():
-    printer(boarders(20, 10), [(15, 10), (5, 3), (1,2)])
+    printer((20, 10), points)
 
 show()
